d4b75e27b8a31b071c4050f5d67b4e84258900d0
Katell Goaer <alt.katellgoaer@gmail.com>
2020-04-23 16:29:45 -0400

    created search bar component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,2 +1,55 @@
 import React from 'react';
 
+import SearchBar from './search_bar';
+import GifList from './gif_list';
+import Gif from './gif';
+
+const GIPHY_API_KEY = 'LaEh73SUOIZexnIPwoYRWBcgxFtCfsxn';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+  }
+
+  search = (query) => {
+    giphy({ apiKey: GIPHY_API_KEY, https: true })
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (err, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
