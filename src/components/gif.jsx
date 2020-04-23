46c65b1eb7ed00f9b937b1b16ef1a53347ef6eed
Katell Goaer <alt.katellgoaer@gmail.com>
2020-04-23 17:39:42 -0400

    created gif and giflist component

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,3 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    this.props.selectGif(this.props.id);
+  }
+
+  render () {
+    const source = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={source} alt="gif" className="gif" onClick={this.handleClick}/>
+    );
+  }
+}
+
 
 export default Gif;
